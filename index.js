86c79bc16b419f45485a3112f0fb9560d97ecec6
AnnaVans <119419794+AnnaVans@users.noreply.github.com>
2022-12-06 19:19:59 +0100

    function formatDay created

--- index.js
+++ index.js
@@ -62,6 +62,14 @@ function convertC(event) {
   temperature.innerHTML = Math.round(celsiusTemp);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
   let forecast = response.data.daily;
 
